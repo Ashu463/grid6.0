fcea2da224cfbfc7e790ee851eafdc661e03b541
ShobitaBhardwaj <135360288+ShobitaBhardwaj@users.noreply.github.com>
2024-08-25 01:29:52 +0530

    Shpping Management APIs added testing Pending

--- backend/api-inventory/src/app.module.ts
+++ backend/api-inventory/src/app.module.ts
@@ -5,9 +5,10 @@ import { AppService } from './app.service';
 import { PmModule } from './product-management/pm.module';
 import { UmModule } from './user-management/um.module';
 import { PrismaService } from './prisma/prisma.service';
+import { SmModule } from './shipping-management/sm.module';
 
 @Module({
-  imports: [PmModule, UmModule],
+  imports: [PmModule, UmModule ,SmModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
