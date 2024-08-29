e1aae50991054a28493b281811ad7dd0f09998a1
Ashu463 <ashukasaudhan971@gmail.com>
2024-08-29 17:49:40 +0530

    test case improved

--- backend/api-inventory/src/app.service.ts
+++ backend/api-inventory/src/app.service.ts
@@ -3,14 +3,5 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   constructor(){}
-  getHello(name : string) {
-    return {
-      success : true, 
-      message : `hello ${name} I am controller`
-    }
-
-  }
-  getlello(): string {
-    return 'Hello World!';
-  }
+  
 }
